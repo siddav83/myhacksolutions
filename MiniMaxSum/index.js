3f3a17153dd01d0ada69c998c80fd8b732fb4a2c
David Siddle <siddav83@gmail.com>
2022-02-15 22:30:31 +0000

    updated

--- MiniMaxSum/index.js
+++ MiniMaxSum/index.js
@@ -2,8 +2,8 @@ const arr = [1,2,3,4,5]
 
 function miniMax(arr) {
   const sorted = arr.sort((a, b) => a - b)
-  const min = sorted.slice(0,3).reduce((i,l) => i + l);
-  const max = sorted.slice(1,4).reduce((i,l) => i + l);
+  const min = sorted.slice(0,4).reduce((i,l) => i + l);
+  const max = sorted.slice(1,5).reduce((i,l) => i + l);
   console.log(min,max)
 }
 
