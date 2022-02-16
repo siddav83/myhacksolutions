aa955d88ea37d7c93f3d9201db2f972958f95c3e
David Siddle <siddav83@gmail.com>
2022-02-16 21:24:21 +0000

    solution - nested loops yo

--- divisibleSumPairs/index.js
+++ divisibleSumPairs/index.js
@@ -1,16 +1,16 @@
-ar = [1,2,3,4,5,6]
+ar = [6,3,[1,3,2,6,1,2]]
 
-function divisibleSumPairs(ar) {
-  let empty = []
-  let div = 3
+function divisibleSumPairs(n, k, ar) {
+  let count = 0
     for ( let i = 0; i < ar.length;i ++) {
       for (let j = 0;j < ar.length; j ++) {
-        if(ar[i]+ar[j]===6) {
-          console.log(ar[i])
-          console.log(ar[j])
+        if(i > j && (ar[i]+ ar[j]) % k === 0) {
+          count +=1
         }
       }
     }
-    // console.log(empty)
-}
+    return count
+  }
 divisibleSumPairs(ar)
+
+1
