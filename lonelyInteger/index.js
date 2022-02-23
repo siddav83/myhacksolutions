eb526f25e9fc69eca2838ffa93bc3e96528296d4
David Siddle <siddav83@gmail.com>
2022-02-23 22:19:27 +0000

    successful

--- lonelyInteger/index.js
+++ lonelyInteger/index.js
@@ -1,8 +1,11 @@
-const a = [1,2,3,4,4,2,1]
+const a = [1,1]
+const b = [1,2,3,4,3,2,1]
 
 function lonelyinteger(a) {
+    const unique = a.filter((uniq) => {
+        return a.indexOf(uniq) ===a.lastIndexOf(uniq)})
+        unique[0]
     // Write your code here
-
 }
 
-console.log(a.lastIndexOf(a[3]))
+lonelyinteger(a)
