4ecd936788a98c2e4411308df70ef116bf2ea442
David Siddle <siddav83@gmail.com>
2022-03-08 13:27:18 +0000

    wrong

--- pangrams/index.js
+++ pangrams/index.js
@@ -1,18 +1,27 @@
 
-const s = "thisismeor";
+const sent = "the quick brown fox fox fox"
 
 function pangrams(s) {
-  const splitSen = s.split('');
-  const alpha = 'tsmep'.split("");
-  const len = alpha.length
-
-  let counter = 0;
-
-  for (i = 0; i < splitSen.length; i++) {
-      for ( j = 0; j < len; j++) {
-          i === j ? counter ++ : counter
-      }
+    
+    const abc = "thequick".split()
+   const uniq = sent.split("").filter((v,i,a)=> { 
+       return a.indexOf(v)==i
+    })
+    const newUniq = uniq.filter(ele => ele !== " ")
+    let counter = 0
+    for ( let i = 0; i < newUniq.length; i++) {
+        for ( let j = 0; j < newUniq.length; j++) {
+            if(i === j) {
+                counter ++
+            }
+        }
     }
     console.log(counter)
 }
-    pangrams(s)
+    pangrams(sent)
+
+// usage example:
+// const myArray = ['a', 1, 'a', 2, '1'];
+// const unique = myArray.filter((v, i, a) => a.indexOf(v) === i);
+
+// console.log(unique)
