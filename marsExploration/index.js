684cc8bfbb03e68e03dadeb73e525b7fbf3979f0
David Siddle <siddav83@gmail.com>
2022-03-20 20:48:06 +0000

    success

--- marsExploration/index.js
+++ marsExploration/index.js
@@ -1,4 +1,22 @@
-function marsExploration(s) {
-    // Write your code here
+const s = "SOSOOSOSOSOSOSSOSOSOSOSOSOS"
 
+function marsExploration(s) {
+    // const s1 = s.match(/.{1,3}/g)
+    const sos = "SOS"
+    let counter = 0
+
+    for (let i = 0; i < s.length;i+=3) {
+        if(s.charAt(i) !== 'S') {
+            counter++
+        }
+        if(s.charAt(i + 1) !== 'O') {
+            counter++
+        }
+        if(s.charAt(i + 2) !== 'O') {
+            counter++
+        }
+    } 
+        return counter
 }
+
+marsExploration(s)
