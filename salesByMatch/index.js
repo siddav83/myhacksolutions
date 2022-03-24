67948034a1f550765c7a1af62858652a9851cfae
David Siddle <siddav83@gmail.com>
2022-03-24 22:14:12 +0000

    learnt about .hasOwnProperty property

--- salesByMatch/index.js
+++ salesByMatch/index.js
@@ -2,10 +2,13 @@ const arr = [1,2,1,2,1,3,2]
 
 function sockMerchant(n, ar) {
     const obj = {}
+    counter = 0
     arr.forEach((ele) => {
-        Object.entries(obj)== "" ? obj[ele] = ele: obj[ele] +=ele
-        // console.log(obj)
-        })
-    console.log(obj)}
+        obj.hasOwnProperty(ele) ? obj[ele] += 1: obj[ele] = 1
+        if(obj[ele] % 2 == 0) {
+            counter ++
+        }})
+    return counter
+}
 
 sockMerchant(arr)
