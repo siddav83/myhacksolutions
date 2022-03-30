a4278034742a117deff058334c2ec2ac733281a9
David Siddle <siddav83@gmail.com>
2022-03-30 21:21:20 +0100

    check later

--- migratoryBirds/index.js
+++ migratoryBirds/index.js
@@ -1,16 +1,18 @@
-const arr = [1, 1, 2, 2, 3]
+const arr = [1, 4, 4, 4, 5, 3]
 
 function migratoryBirds(arr) {
    const obj = {}
    const counter = 0
    arr.forEach(ele => {
        obj.hasOwnProperty(ele) ? obj[ele] +=1: obj[ele] = 1
-   });
+   })
    console.log(obj)
-   let values = Object.keys(obj)
-   let max = Math.max(...values)
-   console.log(max)
-
-}
+//    let values = Object.values(obj)
+//    let max = Math.max(...values)
+//    for ( k in obj) {
+//        if(Object.entries(k) === max) {
+//            console.log(obj[k])
+       
+};
 
 migratoryBirds(arr)
